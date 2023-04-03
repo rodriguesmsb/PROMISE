434a13b87fd896e8a2b9d0c9ca5ab57379c59922
rodriguesmsb <rodriguesmsb@gmail.com>
2023-04-03 14:39:44 -0400

    up on render

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,12 @@
 from app.main import app as application
 
-# app = application
-if __name__ == "__main__":
-    application.run()
+# # app = application
+# if __name__ == "__main__":
+#     application.run()
+
+
+from app.main import app
+
+app = application
+# if __name__ == "__main__":
+#     main.app.run()
