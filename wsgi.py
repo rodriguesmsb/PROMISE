a071b2e5b8a61cd9fa88f9b4c0df33a8411d1b75
rodriguesmsb <rodriguesmsb@gmail.com>
2023-04-26 13:42:49 -0400

    deploy on render

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from app.main import app as application
 #from app.main import app
 
-#app = application
+app = application
 
-# app = application
-if __name__ == "__main__":
-    application.run()
+# # app = application
+# if __name__ == "__main__":
+#     application.run()
